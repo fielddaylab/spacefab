becff65d157b2010042489c901bea728a3ac13c7
levimhuillet <levimhuillet@gmail.com>
2026-03-10 19:55:31 -0500

    progress on minigame zones

---
+++ Assets/Modules/Overarching/Code/MinigameZone.cs
@@ -0,0 +1,13 @@
+using BeauUtil.UI;
+using FieldDay.Components;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceFab.Overarching
+{
+    public class MinigameZone : BatchedComponent
+    {
+        public PointerListener PointerListener;
+    }
+}
