78e0a48ea0dfe74e383d79c0461a9e7ede609edd
cosums <114266944+cosums@users.noreply.github.com>
2026-05-08 12:33:23 -0500

    Initial ui blocking

--- Assets/Shared/Code/Contracts/ContractUI.cs
+++ Assets/Shared/Code/Contracts/ContractUI.cs
@@ -7,6 +7,21 @@ namespace SpaceFab {
     public class ContractUI : MonoBehaviour
     {
         public TMP_Text Title;
+        public TMP_Text Description;
+        public Transform DurationParent, ProfitParent;
+        public GameObject DurationElement, ProfitElement;
+
+        public void ShowElements(int elements, Transform parent, GameObject elementObject)
+        {
+            for (int i = 0; i < elements; i++)
+            {
+                GameObject element = Instantiate(elementObject);
+                elementObject.transform.SetParent(parent);
+                elementObject.transform.localScale = Vector3.one;
+
+                Debug.Log("Working?");
+            }
+        }
     }
 
     public static class ContractUtility
@@ -16,10 +31,16 @@ namespace SpaceFab {
             if (def == null)
             {
                 ui.Title.SetText(string.Empty);
+                ui.Description.SetText(string.Empty);
             }
             else
             {
                 ui.Title.SetText(def.Title());
+                ui.Description.SetText(def.Description());
+                Debug.Log("Loading some stuff!");
+                ui.ShowElements(def.ExpectedDuration(), ui.DurationParent, ui.DurationElement);
+                ui.ShowElements(def.ExpectedProfit(), ui.ProfitParent, ui.ProfitElement);
+                Debug.Log("Loaded some stuff!");
             }
         }
     }
