cd0f18d36ce96ae0bae18f07c5663111823a76ca
Autumn <beauchesne@wisc.edu>
2026-05-14 11:29:48 -0500

    * disable raycasts on progress meter cells * track wasm heap size

--- Assets/FieldDay/Sys/Memory/Mem.cs
+++ Assets/FieldDay/Sys/Memory/Mem.cs
@@ -9,11 +9,17 @@ namespace FieldDay.Memory {
         static internal MemoryMgr Mgr;
 
         static public event GCNotifyDelegate OnGarbageCollectionOccurred;
+        static public event HeapSizeChangedDelegate OnHeapSizeChanged;
 
         static internal void InvokeGCOccurred(int mask) {
             OnGarbageCollectionOccurred?.Invoke(mask);
         }
+
+        static internal void InvokeHeapSizeChanged(long newHeapSize) {
+            OnHeapSizeChanged?.Invoke(newHeapSize);
+        }
     }
 
     public delegate void GCNotifyDelegate(int generationMask);
+    public delegate void HeapSizeChangedDelegate(long newHeapSize);
 }
