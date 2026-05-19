39002f32da452e515ac252a362ead7096568b531
cosums <114266944+cosums@users.noreply.github.com>
2026-05-19 14:23:57 -0500

    Reapply "Merge branch 'ion-implanter' into fabrication"
    This reverts commit 93d73962bd74ccdab0b357925dc9ebcb1fccbe1e.

---
+++ Assets/Modules/Fabrication/Code/Microgames/IonPatternData.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IonPatternData : MonoBehaviour
+{
+    public IonFillRenderer[] IonFillRenderers;
+
+    private int m_TotalPoints, m_FilledPoints;
+
+    public bool CompletelyFilled => m_TotalPoints == m_FilledPoints;
+
+    public void SetupRenderers(float density, float fillRadius)
+    {
+        foreach (var i in IonFillRenderers)
+        {
+            m_TotalPoints += i.Setup(density, fillRadius);
+            i.IsRendering = true;
+        }
+    }
+
+    public void ProcessWork()
+    {
+        m_FilledPoints = 0;
+        
+        foreach (var i in IonFillRenderers)
+        {
+            m_FilledPoints += i.ProcessWork();
+        }
+
+        Debug.Log("Total points:" + m_TotalPoints + "Filled points: " + m_FilledPoints);
+    }
+
+    // decouple from process work as this must be called during exit
+    public void PerformRendering()
+    {
+        foreach (var i in IonFillRenderers)
+        {
+            i.PerformRendering();
+        }
+    }
+}
