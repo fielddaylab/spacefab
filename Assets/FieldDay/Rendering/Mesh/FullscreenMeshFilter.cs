fa8406a8e195a7235190ecd7d304a85842651631
Autumn <beauchesne@beauprime.com>
2026-07-07 16:42:43 -0500

    * ensure FullscreenMeshFilter does not create mesh during build

--- Assets/FieldDay/Rendering/Mesh/FullscreenMeshFilter.cs
+++ Assets/FieldDay/Rendering/Mesh/FullscreenMeshFilter.cs
@@ -15,6 +15,12 @@ namespace FieldDay.Rendering {
     [ExecuteAlways]
     public sealed class FullscreenMeshFilter : MonoBehaviour {
         private void OnEnable() {
+#if UNITY_EDITOR
+            if (EditorApplication.isPlayingOrWillChangePlaymode || BuildPipeline.isBuildingPlayer) {
+                return;
+            }
+#endif // UNITY_EDITOR
+
             GetComponent<MeshFilter>().sharedMesh = FullscreenMesh.CreateMesh();
         }
     }
