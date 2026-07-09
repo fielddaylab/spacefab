70051ca48d130fd2fb5eacd888672fdb1d923f31
soliloquymoon <soliloquy.moon@gmail.com>
2026-07-09 17:49:09 -0500

    fix chamber activation

---
+++ Assets/Modules/Research/Code/UI/ChamberButton.cs
@@ -0,0 +1,12 @@
+using FieldDay.Components;
+using FieldDay.UI;
+using UnityEngine.UI;
+
+namespace SpaceFab.Research
+{
+    public class ChamberButton : BatchedComponent
+    {
+        public CursorHint Cursor;
+        public Image Image;
+    }
+}
