5e0887ff68633150b1c9879fd9b5194ad8c86eb7
Autumn <beauchesne@beauprime.com>
2026-08-07 16:13:08 -0500

    * new overarching camera poses

--- Assets/Modules/Overarching/Code/OverarchingCamera.cs
+++ Assets/Modules/Overarching/Code/OverarchingCamera.cs
@@ -1,7 +1,11 @@
 using BeauUtil;
+using BeauUtil.Debugger;
 using FieldDay;
 using FieldDay.Scenes;
+using FieldDay.Scripting;
 using FieldDay.SharedState;
+using Leaf;
+using Leaf.Runtime;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -20,6 +24,17 @@ namespace SpaceFab.Overarching
         IEnumerator<WorkSlicer.Result?> IScenePreload.Preload() {
             OverarchingRenderUtility.SwitchPose(this, DefaultPose);
             return null;
+        }
+    }
+
+    static public class OverarchingCameraScripting {
+        [LeafMember("OverarchingSwapCamera")]
+        static private void Leaf_SwapCameraPose(StringHash32 poseId) {
+            ScriptActor actor = ScriptUtility.FindActor(poseId);
+            Assert.NotNullOrDestroyed(actor, "No actor with id '{0}'", poseId);
+            actor.TryGetComponent(out OverarchingRenderPose pose);
+            Assert.NotNullOrDestroyed(pose, "Actor '{0}' is not a valid camera pose", poseId);
+            OverarchingRenderUtility.SwitchPose(Find.State<OverarchingCamera>(), pose);
         }
     }
 }
