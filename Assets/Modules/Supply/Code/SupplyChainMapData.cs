0ae1ff05853b3737ea746992240e5b0d6ff63178
levimhuillet <levimhuillet@gmail.com>
2026-08-11 12:01:58 -0500

    update level 1 supply map

--- Assets/Modules/Supply/Code/SupplyChainMapData.cs
+++ Assets/Modules/Supply/Code/SupplyChainMapData.cs
@@ -9,13 +9,13 @@ namespace SpaceFab.Supply
     public class SupplyChainMapData : ScriptableObject {
         [Serializable]
         public struct NodeData {
-            public StringHash32 Name;
+            public SerializedHash32 Name;
             public Vector2 Position;
         }
 
         [Serializable]
         public struct NodeOverride {
-            public StringHash32 Name;
+            public SerializedHash32 Name;
             public int Time;
             public int Cost;
             public int Risk;
