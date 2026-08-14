1ca76b5f202dd1a701a68ce79086b7c3c8bd656e
cosums <114266944+cosums@users.noreply.github.com>
2026-08-14 11:41:12 -0500

    Finish supply progress meter

---
+++ Assets/ProgressMeterSlot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FieldDay.Components;
+using UnityEngine.UI;
+
+public class ProgressMeterSlot : BatchedComponent
+{
+    public Image SlotImage;
+    public Image SlotOverlay;
+    public Sprite Empty, Fill, Red;
+
+
+    public void SetState(bool filled)
+    {
+        
+    }
+}
