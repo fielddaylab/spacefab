983cd2ca24ddbc0ad3a060c9218ab555605886a5
REDY-a <alice.zqr233@gmail.com>
2026-08-31 17:27:02 -0500

    disable transistor draws under inputs/outputs

--- Assets/Modules/Design/Code/Tool Mode/DrawUtility.cs
+++ Assets/Modules/Design/Code/Tool Mode/DrawUtility.cs
@@ -100,7 +100,13 @@ namespace SpaceFab.Design
             // disallow drag from inputs/outputs on transistor layer
             if (type == CellType.NTransistor || type == CellType.PTransistor)
             {
-                if (fromCell.CellType == CellType.Input || fromCell.CellType == CellType.Input)
+                if (fromCell.CellType == CellType.Input || fromCell.CellType == CellType.Output)
+                {
+                    ToolModeUtility.TerminateDrag(toolModeState);
+                    return;
+                }
+
+                if (!CanDrawNode(gridState, toolModeState.ActiveLayer, toCell, gridPos))
                 {
                     ToolModeUtility.TerminateDrag(toolModeState);
                     return;
@@ -125,6 +131,16 @@ namespace SpaceFab.Design
             // save changes
             GridLayerUtility.SetCellAndUpdateVisuals(visualState, layer, toolModeState.LastKnownDragCoord, fromCell);
             GridLayerUtility.SetCellAndUpdateVisuals(visualState, layer, gridPos, toCell);
+            }
+        public static bool CanDrawNode(GridStackState gridState, StackLayer activeLayer, GridCell cell, Vector2Int gridPos)
+        {
+            if (cell.CellType == CellType.Input || cell.CellType == CellType.Output) return false;
+
+            var linkedLayer = gridState.GridStack.GridLayers[(int)GridStackUtility.GetOppositeLayer(activeLayer)];
+            var linkedCell = GridLayerUtility.GetCell(linkedLayer, gridPos);
+            if (linkedCell.CellType == CellType.Input || linkedCell.CellType == CellType.Output) return false;
+
+            return true;
         }
     }
 }
